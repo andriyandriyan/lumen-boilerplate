ce141d048526681c1fb6bc3e14aef1fefaa7f5c9
andriyandriyan <andriy.andriyan1@gmail.com>
2019-08-19 00:09:28 +0700

    Create simple route for auth

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use \Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,16 @@
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->post('/login', function (Request $request) {
+    $token = app('auth')->attempt($request->only('email', 'password'));
+    return response()->json(compact('token'));
+});
+
+$router->group(['middleware' => 'auth'], function () use ($router) {
+    $router->get('/user', function () {
+        $user = app('auth')->user();
+        return response()->json(compact('user'));
+    });
 });
